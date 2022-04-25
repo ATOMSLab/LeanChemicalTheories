35513e1743aa22eee2543e7bfdffb28435ee88fb
Samiha Sharlin <70553929+ssharlin@users.noreply.github.com>
2022-04-25 15:13:30 -0400

    Update Charles's Law.lean

--- src/Thermodynamics/Charles's Law.lean
+++ src/Thermodynamics/Charles's Law.lean
@@ -1,9 +1,7 @@
-import analysis.complex.basic
-namespace nnreal
-open_locale nnreal big_operators
+import data.real.basic
 
 -- Variables
-lemma Charles (P1 P2 V1 V2 T1 T2 n1 n2 R : ℝ≥0 )
+lemma Charles (P1 P2 V1 V2 T1 T2 n1 n2 R : ℝ)
 
 -- Assumptions
 (a1: P1*V1=n1*R*T1)  -- Gas 1 is ideal
