9ac03a8f8944facc3b0a22423e7c2a3423acd64e
Octravon <33993023+Octravon@users.noreply.github.com>
2022-05-04 16:23:31 -0400

    Update Boyle'sLaw.lean
    Added my other proof of Boyle's

--- src/Thermodynamics/Boyle'sLaw.lean
+++ src/Thermodynamics/Boyle'sLaw.lean
@@ -1,4 +1,5 @@
 import data.real.basic
+import data.real.nnreal
 
 -- Variables
 lemma Boyle (P1 P2 V1 V2 T1 T2 n1 n2 R : ℝ )
@@ -21,3 +22,23 @@ begin
     --and exact a1,   
 end
 
+open nnreal
+local notation `ℝ≥0` := {r : ℝ // 0 ≤ r}
+theorem Boyle's_Law 
+{k : ℝ≥0}
+(h1 : ∃(P V : ℝ≥0), P*V = k)
+:
+∃ (P1 P2 V1 V2 : ℝ≥0), P1*V1 = P2*V2 
+:=
+begin
+    have h2 : ∃(P V : ℝ≥0), P*V = k,
+    exact h1,
+    cases h1 with P1 h1,
+    cases h1 with V1 h1,
+    cases h2 with P2 h2,
+    cases h2 with V2 h2,
+    rw ← h1 at h2,
+    use P1, use P2, use V1, use V2,
+    symmetry,
+    exact h2,
+end
