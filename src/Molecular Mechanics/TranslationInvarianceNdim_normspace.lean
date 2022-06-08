653f1f1d4c0aca3f26c74956bb5fcee34729276d
Samiha Sharlin <70553929+ssharlin@users.noreply.github.com>
2022-06-08 15:30:58 -0400

    Add files via upload
    Translation invariance_Ndim_normedspace

---
+++ src/Molecular Mechanics/TranslationInvarianceNdim_normspace.lean
@@ -0,0 +1,32 @@
+import analysis.normed.group.basic
+import analysis.normed_space.basic
+variables {n: ℕ }
+local notation `ℝ^n ` := fin (n) → ℝ
+
+theorem norm_trans_invar 
+
+:
+∀ (n : ℕ ) (x y t : ℝ^n), has_dist.dist x y = has_dist.dist (x+t) (y+t)
+:=
+begin
+  repeat {intro},
+  repeat {rw real.dist_eq},
+  simp,
+end
+
+
+lemma LJInvariance (x y t : ℝ^n) (r rt a b E Et: ℝ)
+( a1: r = has_dist.dist x y)  -- pairwise distance
+( a2: rt = has_dist.dist (x+t) (y+t)  ) --- pairwise distance after translation
+( a3: E = a/r^12 - b/r^6 ) -- LJ potential
+( a4: Et = a/rt^12 - b/rt^6 ) -- LJ potential after translation t
+:
+--Conjecture
+E = Et
+:=
+begin
+  have ht: r=rt,
+  finish using norm_trans_invar n,
+  rw ht at a3,
+  exact (rfl.congr (eq.symm a4)).mp a3,  
+end
