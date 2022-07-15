9dcf70ef80142c53562fd2ade015c50bee4a3cc2
TylerJosephson <53125027+TylerJosephson@users.noreply.github.com>
2022-07-14 22:50:22 -0400

    Add comments to Langmuir

--- src/Adsorption/langmuirKinetics.lean
+++ src/Adsorption/langmuirKinetics.lean
@@ -2,7 +2,7 @@ import data.real.basic
 
 theorem kineticDerivLangmuir
 [comm_group ℝ]
-(theta k_eq P_A r_ad r_d k_ad k_d A_ad S_0 S : ℝ) -- declar all variables to be real numbers
+(theta k_eq P_A r_ad r_d k_ad k_d A_ad S_0 S : ℝ) -- declare all variables to be real numbers
 -- Premises
 (h1 : r_ad = k_ad*P_A*S) -- Adsorption rate expression
 (h2 : r_d = k_d*A_ad) -- Desorption rate expression
@@ -15,7 +15,7 @@ theorem kineticDerivLangmuir
 (h9 : k_d ≠ 0)
 : 
 theta = k_eq*P_A/(1+k_eq*P_A) := -- Langmuir's adsorption law
-begin
+begin -- Proof starts here
   rw [h1, h2] at h3,
   rw [h6, h5, h4],
   field_simp,
