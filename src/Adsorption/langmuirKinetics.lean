6761a9edce662a6e18d51db0d4c40550b83a1ef6
TylerJosephson <53125027+TylerJosephson@users.noreply.github.com>
2022-07-14 22:49:21 -0400

    Add comments to Langmuir

--- src/Adsorption/langmuirKinetics.lean
+++ src/Adsorption/langmuirKinetics.lean
@@ -2,14 +2,15 @@ import data.real.basic
 
 theorem kineticDerivLangmuir
 [comm_group ℝ]
-(theta k_eq P_A r_ad r_d k_ad k_d A_ad S_0 S : ℝ) --premises
+(theta k_eq P_A r_ad r_d k_ad k_d A_ad S_0 S : ℝ) -- declar all variables to be real numbers
+-- Premises
 (h1 : r_ad = k_ad*P_A*S) -- Adsorption rate expression
 (h2 : r_d = k_d*A_ad) -- Desorption rate expression
 (h3 : r_ad = r_d) -- Equilibrium assumption
 (h4 : k_eq = k_ad/k_d) -- Definition of adsorption constant
 (h5 : S_0 = S + A_ad) -- Site balance
 (h6 : theta = A_ad/S_0) -- Definition of fractional coverage
-(h7 : S + A_ad ≠ 0) 
+(h7 : S + A_ad ≠ 0)
 (h8 : k_d + k_ad * P_A ≠ 0)
 (h9 : k_d ≠ 0)
 : 
