65d3986ac68845422b02a6fffc84fa36ac7d0c9f
Octravon <maxbobbin916@gmail.com>
2022-07-27 15:45:39 -0400

    delted boyels law 2

--- src/Thermodynamics/Boyle'sLaw.lean
+++ src/Thermodynamics/Boyle'sLaw.lean
@@ -22,23 +22,3 @@ begin
     --and exact a1,   
 end
 
-open nnreal
-local notation `ℝ≥0` := {r : ℝ // 0 ≤ r}
-theorem Boyle's_Law_2
-{k : ℝ≥0}
-(h1 : ∃(P V : ℝ≥0), P*V = k)
-:
-∃ (P1 P2 V1 V2 : ℝ≥0), P1*V1 = P2*V2 
-:=
-begin
-    have h2 : ∃(P V : ℝ≥0), P*V = k,
-    exact h1,
-    cases h1 with P1 h1,
-    cases h1 with V1 h1,
-    cases h2 with P2 h2,
-    cases h2 with V2 h2,
-    rw ← h1 at h2,
-    use P1, use P2, use V1, use V2,
-    symmetry,
-    exact h2,
-end
