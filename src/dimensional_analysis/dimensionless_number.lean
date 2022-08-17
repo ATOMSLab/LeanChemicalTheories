56de8c1910c24b924c8aab15cefbb3ccda307f45
Samiha Sharlin <70553929+ssharlin@users.noreply.github.com>
2022-08-16 20:56:15 -0400

    imports to dimensionless numbers

--- src/dimensional_analysis/dimensionless_number.lean
+++ src/dimensional_analysis/dimensionless_number.lean
@@ -1,7 +1,28 @@
+import dimensional_analysis.basic
+
 /-! # Proof of dimensionless numbers
-We defined gravitational acceleration (g), coefficient of thermal expansion β, kinematic viscosity (ν),
-surfact temperature (T_s), bulk temperature (T_inf), thermal diffusivity (α) to demonstrate Grashof and Prandtl
-numbers are dimensionless -/
+We demonstrate Grashof and Prandtl numbers are dimensionless by expressing the variables in their
+fundamental units defined in our `dimensionsal_analysis.basic` system.
+
+Here:
+- `g` is gravitational acceleration 
+- `β` is coefficient of thermal expansion 
+- `ν` is kinematic viscosity 
+- `T_s` is surfact temperature
+- `T_inf` is bulk temperature 
+- `α` is thermal diffusivity 
+
+-/
+
+namespace dimension
+
+local notation `L` := dimension.length
+local notation `M` := dimension.mass
+local notation `N` := dimension.amount_of_substance
+local notation `I` := dimension.electric_current
+local notation `θ` := dimension.absolute_temperature
+local notation `T` := dimension.time
+local notation `J` := dimension.luminous_intensity
 
 def acceleration := L/T^2
 local notation `g`:= acceleration
@@ -21,6 +42,9 @@ local notation `T_inf`:= bulk_temp
 def therm_diffusivity := L^2/T
 local notation `α` := therm_diffusivity
 
+def diameter := L
+local notation `L` := diameter
+
 theorem dimensionless_grashof: g*β*(T_s-T_inf)*(L^3)/(ν^2)=1 :=
 begin
   field_simp [acceleration,coeff_therm_exp,surface_temp,bulk_temp,diameter,kinematic_viscosity,length,absolute_temperature,time],
@@ -31,3 +55,5 @@ theorem dimensionless_prandtl: ν/α =1:=
 begin
   field_simp [kinematic_viscosity,therm_diffusivity],
 end
+
+end dimension
