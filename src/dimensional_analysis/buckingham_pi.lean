7719befd0eee41b5d23a306d0091a6fd7982c3e6
Octravon <maxbobbin916@gmail.com>
2022-12-10 13:22:21 -0500

    Work on new B-Pi theorem

--- src/dimensional_analysis/buckingham_pi.lean
+++ src/dimensional_analysis/buckingham_pi.lean
@@ -1,8 +1,20 @@
 import dimensional_analysis.basic3
 import linear_algebra.dimension
+import data.matrix.rank
+import data.fin.vec_notation
 
 
 
+variables {α : Type*} {n : ℕ} 
 
-def number_of_dimensionless_parameters {α} (f : dimension α →ₗ[ℚ] dimension α)
 
+noncomputable def number_of_dimensionless_parameters (M : matrix α (fin n) ℚ) := n - M.rank
+open dimension
+
+example : number_of_dimensionless_parameters !![length system1 system1.length, time system1 system1.length, velocity system1 system1.length; length system1 system1.time, time system1 system1.time, velocity system1 system1.time] = 1 :=
+begin
+  simp [dimension.velocity, number_of_dimensionless_parameters],
+  rw [length_at_length_eq_one, length_at_time_eq_zero, time_at_time_eq_one, time_at_length_eq_zero],
+  norm_num,
+  
+end
