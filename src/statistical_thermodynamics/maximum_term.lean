03d6b30b7c2a1ec165c1e44787f96251e89355b0
Octravon <maxbobbin916@gmail.com>
2023-01-17 10:37:44 -0500

    moved into src

--- src/statistical_thermodynamics/maximum_term.lean
+++ src/statistical_thermodynamics/maximum_term.lean
@@ -8,7 +8,8 @@ open_locale big_operators
 theorem finset.mem_le_pos_sum {α : Type*} {β : Type*} [linear_ordered_add_comm_group β] (f : α → β) (s : finset α) (h1 : ∀ x, x ∈ s → 0 < f x) 
 :  ∀ y (H : y ∈ s), f y ≤ ∑ x in s, f x :=
 begin
-  
-  
+  apply finset.single_le_sum,
+  intros x h,
+  apply le_of_lt (h1 x h),
 
 end
